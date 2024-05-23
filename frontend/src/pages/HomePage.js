19bc06b01a93c24f90aeb04f342b9b88bffe9872
aaronanderson7 <107898465+aaronanderson7@users.noreply.github.com>
2024-05-23 12:22:29 -0700

    Final Frontend Commit - with Jonathan's load bar.

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,14 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+
+// Icons
+import { FaRegTrashCan } from "react-icons/fa6";
+import { GrUploadOption } from "react-icons/gr";
+
+
 const FileUpload = () => {
-    const [file, setFile] = useState('');
-    const [summary, setSummary] = useState(null);
+    const [file, setFile] = useState(null);
     const navigate = useNavigate();
     const [loading, setloading] = useState(false);
+    const inputRef = useRef();
 
     const handleFileChange = (e) => {
-      setFile(e.target.files[0]);
+      if (e.target.files && e.target.files.length > 0) {
+        setFile(e.target.files[0]);
+      }
+    };
+
+    const onChooseFile = () => {
+      inputRef.current.click();
+    };
+
+    const removeFile = () => {
+      setFile(null);
     };
   
     const handleSubmit = async (e) => {
@@ -21,7 +37,7 @@ const FileUpload = () => {
       // Checks file size
       if (file.size > 2000000 ) {
         // window.alert(file.size);
-        window.alert("File is too large");
+        window.alert("File is too large. Please enter files with size < 2000 kb.");
         return;
       }
 
@@ -39,11 +55,7 @@ const FileUpload = () => {
   
         if (response.ok) {
             const data = await response.json();
-            console.log(data);
             console.log(data.summary);
-            // setSummary(data.summary);
-            // console.log(summary);
-
             navigate('/summary', {state: data.summary});
         } else {
             console.error('Failed to upload file or retrieve summary.');
@@ -56,16 +68,32 @@ const FileUpload = () => {
     return (
       <>
         <div class="upload-container">
-          <h1>Upload PDF</h1>
-          <p>Maximum file size: 2000 kb</p>
-          <div class="upload-form">
-            <form onSubmit={handleSubmit}>
-              <input type="file" onChange={handleFileChange} accept=".pdf" />
-              <button type="submit">Upload</button>
-            </form>
+          <p></p>
+          <p>Please upload a scientific article in the form of a pdf below and press the Submit button for a SummarSci summary. The maximum file size is 2000 kb.</p>
+          
+          <input type="file" ref = {inputRef} style = {{display: "none"}} onChange = {handleFileChange} accept=".pdf"/>
+
+          <button class = "file-btn" onClick = {onChooseFile}>
+            <span> <GrUploadOption/> </span> Upload File
+          </button>
+
+          {file && <div class = "selected-file">
+          <p>{file.name}</p>
+          
+          <button onClick = {removeFile}>
+            <span><FaRegTrashCan/></span>
+          </button>
           </div>
+          }
+          
+          <form onSubmit={handleSubmit} className = "submit">
+            <button type="submit">Submit</button>
+          </form>
+
+
           {loading ? <div class="loader"></div>: null}
           {loading ? <p>Loading...</p>: null}
+
         </div>
       </>
     );
